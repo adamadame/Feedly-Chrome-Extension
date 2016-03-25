4c0bd24cce2579e3f0517455e4a6649bf2502022
Adam Adame <adam.adame19@gmail.com>
2016-03-25 01:48:26 -0700

    Attempts to remove an HTML element.
    Here I try to remove HTML elements with a very limited knowledge of
    JavaScript.

--- js/bloatfree.js
+++ js/bloatfree.js
@@ -1,12 +1,8 @@
-// Checking page title
-if (document.title.indexOf("Today") != -1) {
-    //Creating Elements
-    var btn = document.createElement("BUTTON")
-    var t = document.createTextNode("CLICK ME");
-    btn.appendChild(t);
-    //Appending to DOM
-    document.body.appendChild(btn);
-}
+var parent = document.getElementById("feedlyCenter");
+var child = document.getElementById("feedlyFrame");
+parent.removeChild(child);
+//Appending to DOM
+// document.body.appendChild(parent);
 
 // Sample Code
 /* // Checking page title
